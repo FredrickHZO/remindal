eb9bd02c2035feb5ab10dc0f337a9853c7acbd50
FredrickHZO <federico.armagno97@gmail.com>
2024-05-09 18:16:16 +0200

    Update main

--- back-end/main.go
+++ back-end/main.go
@@ -4,33 +4,24 @@ import (
 	_ "embed"
 	"log"
 	"net/http"
-	"remindal/res"
+	"remindal/routes"
 
 	"github.com/gorilla/mux"
 )
 
 var (
-	//go:embed token
-	embed_test string
-	port       string = ":8080"
+	router *mux.Router = mux.NewRouter()
+	port   string      = ":8080"
 )
 
-func handleHome(w http.ResponseWriter, r *http.Request) {
-	if id := r.URL.Query().Get("id"); id == "" {
-		res.Err(w, http.StatusBadRequest)
-		return
-	}
-	res.Ok(w)
+func handleTestRoutes() {
+	router.HandleFunc("/user", routes.GetUserHandle).Methods("GET")
+	router.HandleFunc("/user", routes.PutUserHandle).Methods("PUT")
+	router.HandleFunc("/user/list", routes.GetUsersListHandle).Methods("GET")
 }
 
 func main() {
-	log.Print(embed_test)
-	router := mux.NewRouter()
-
-	router.HandleFunc("/", handleHome)
-	router.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
-		res.Err(w, http.StatusBadRequest)
-	})
+	handleTestRoutes()
 
 	log.Print("server will be listening on port", port)
 	log.Fatal(http.ListenAndServe(port, router))
