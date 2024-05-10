bb61f667af118361b5929367d6ffac20a0bb1eb9
FredrickHZO <federico.armagno97@gmail.com>
2024-05-10 10:24:48 +0200

    Add calendar schema

--- back-end/database/schemas.go
+++ back-end/database/schemas.go
@@ -1,8 +1,23 @@
 package database
 
 type UserSchema struct {
-	Email   string `bson:"_id" json:"email"`
-	Name    string `bson:"name,omitempty"`
-	Surname string `bson:"surname,omitempty"`
-	Age     int    `bson:"age,omitempty"`
+	Email    string `bson:"_id" json:"email"`
+	Password string `bson:"password"`
+	Name     string `bson:"name,omitempty"`
+	Surname  string `bson:"surname,omitempty"`
+	Age      uint8  `bson:"age,omitempty"`
+}
+
+type Date struct {
+	Labels []string `bson:"label"`
+	Type   string   `bson:"type"`
+
+	Year  string `bson:"year"`
+	Month string `bson:"month"`
+	Day   string `bson:"day"`
+}
+
+type CalendarSchema struct {
+	Alias string `bson:"alias"`
+	Dates []Date `bson:"dates"`
 }
