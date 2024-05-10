a1890ac392a0ced1273218e5a2f4e495549a6f02
FredrickHZO <federico.armagno97@gmail.com>
2024-05-10 10:25:33 +0200

    Throw error on failed connection to database

--- back-end/database/connection.go
+++ back-end/database/connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	_ "embed"
+	"errors"
 	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,25 +12,35 @@ import (
 
 var (
 	//go:embed mongouri
-	mongoURI        string
-	DB_NAME         = "calendars"
-	USER_COLLECTION = "users"
+	mongoURI            string
+	DB_NAME             = "remindalDB"
+	CALENDAR_COLLECTION = "calendar"
+	USER_COLLECTION     = "users"
+
+	errCouldNotReachDatabase = errors.New("could not reach database")
 )
 
 func CloseConnection(client *mongo.Client) {
 	err := client.Disconnect(context.Background())
 	if err != nil {
-		log.Fatal("database - CloseConnection ", err)
+		log.Println("database - CloseConnection ", err)
 	}
 }
 
-func OpenConnection() *mongo.Client {
+/*
+Opens connection to the Remindal database.
+
+Should the attempt to establish a connection fail, returns [nil] for the client and
+[errCouldNotReachDatabase] error.
+*/
+func OpenConnection() (*mongo.Client, error) {
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(mongoURI).SetServerAPIOptions(serverAPI)
 
 	client, err := mongo.Connect(context.Background(), opts)
 	if err != nil {
-		log.Fatal("database - OpenConnection ", err)
+		log.Println("database - OpenConnection ", err)
+		return nil, errCouldNotReachDatabase
 	}
-	return client
+	return client, nil
 }
