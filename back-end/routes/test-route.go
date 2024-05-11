c7d96320338e6f543c395a516f80121c410ddaf7
FredrickHZO <federico.armagno97@gmail.com>
2024-05-11 11:17:30 +0200

    Update decodeRequestBody logic

--- back-end/routes/test-route.go
+++ back-end/routes/test-route.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
+	"log"
 	"net/http"
 	"remindal/database"
 	"remindal/res"
@@ -19,7 +20,7 @@ var (
 	EMAIL_KEY_DB   = "_id"
 	EMAIL_KEY_JSON = "email"
 
-	errInternalServerError = errors.New("internal server error")
+	errInternalServerError = errors.New(http.StatusText(http.StatusInternalServerError))
 	errItemAlreadyPresent  = errors.New("item already in database")
 	errNoDocumentsFound    = errors.New("no documents found")
 	errNoEmailProvided     = errors.New("no email provided")
@@ -27,10 +28,10 @@ var (
 )
 
 // gets the list of users in the database
-func GetUsersListHandle(w http.ResponseWriter, r *http.Request) {
+func GetUsersListHandler(w http.ResponseWriter, r *http.Request) {
 	client, err := database.OpenConnection()
 	if err != nil {
-		res.Err(w, err, 500)
+		res.Err(w, err, http.StatusBadRequest)
 		return
 	}
 	defer database.CloseConnection(client)
@@ -38,80 +39,95 @@ func GetUsersListHandle(w http.ResponseWriter, r *http.Request) {
 	users := client.Database(database.DB_NAME).Collection(database.USER_COLLECTION)
 	cursor, err := users.Find(CTX, bson.D{{}})
 	if err != nil {
-		res.Err(w, err, 400)
+		res.Err(w, err, http.StatusInternalServerError)
 		return
 	}
 
 	var retrievedUserList []database.UserSchema
 	if err := cursor.All(CTX, &retrievedUserList); err != nil {
 		// this should also be errNoDocumentsFound?
-		res.Err(w, errInternalServerError, 500)
+		res.Err(w, errInternalServerError, http.StatusInternalServerError)
 		return
 	}
 	res.Ok(w, retrievedUserList)
 }
 
 // gets a single user from database
-func GetUserHandle(w http.ResponseWriter, r *http.Request) {
+func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	client, err := database.OpenConnection()
 	if err != nil {
-		res.Err(w, err, 500)
+		res.Err(w, err, http.StatusInternalServerError)
 		return
 	}
 	defer database.CloseConnection(client)
 
 	email := r.URL.Query().Get(EMAIL_KEY_JSON)
 	if email == "" {
-		res.Err(w, errNoEmailProvided, 400)
+		res.Err(w, errNoEmailProvided, http.StatusBadRequest)
 		return
 	}
 
 	retrievedUser, err := getUser(client, email)
 	if err != nil {
-		res.Err(w, err, 400)
+		res.Err(w, err, http.StatusInternalServerError)
 		return
 	}
 	res.Ok(w, retrievedUser)
 }
 
-// decodes the request body - THIS IS A GENERIC FUNCTION, MUST HAVE ITS OWN PLACE INSIDE A PACKAGE
-func decodeRequestBody(b io.Reader) ([]byte, error) {
-	body, err := io.ReadAll(b)
-	if err != nil {
-		return nil, err
-	}
-	if len(body) <= 0 {
-		return nil, errNoBodyProvided
-	}
-	return body, nil
-}
-
 // handles the put request to add a new user to database
-func PutUserHandle(w http.ResponseWriter, r *http.Request) {
+func PutUserHandler(w http.ResponseWriter, r *http.Request) {
 	client, err := database.OpenConnection()
 	if err != nil {
-		res.Err(w, err, 500)
+		res.Err(w, err, http.StatusInternalServerError)
 		return
 	}
 	defer database.CloseConnection(client)
 
-	body, err := decodeRequestBody(r.Body)
+	body, statusCode, err := decodeRequestBody(r.Body)
 	if err != nil {
-		res.Err(w, err, 400)
+		res.Err(w, err, statusCode)
 		return
 	}
 
 	var newuser database.UserSchema
 	if err := json.Unmarshal(body, &newuser); err != nil {
-		res.Err(w, errInternalServerError, 500)
+		log.Println("PutUserHandle - json.Unmarshal ", err)
+		res.Err(w, errInternalServerError, http.StatusInternalServerError)
 		return
 	}
 
 	if err := putNewUser(client, newuser); err != nil {
-		res.Err(w, err, 400)
+		res.Err(w, err, http.StatusBadRequest)
 		return
 	}
 	res.Ok(w, nil)
+}
+
+/*
+Decodes the body in the HTTP request and returns it as a byte array.
+
+If there is an error trying to read the request body, returns [nil] for the byte array, [InternalServerError]
+as code status and [errInternalServerError] as error.
+
+Should the body be empty, returns [nil] for the byte array, [StatusBadRequest] as code status and
+[errNoBodyProvided] as error.
+
+In case the body is correctly read it will return the body as a byte array, [StatusOK] as code status
+and [nil] for the error.
+
+TODO: move function.
+*/
+func decodeRequestBody(b io.Reader) ([]byte, int, error) {
+	body, err := io.ReadAll(b)
+	if err != nil {
+		log.Println("decodeRequestBody - io.ReadAll ", err)
+		return nil, http.StatusInternalServerError, errInternalServerError
+	}
+	if len(body) <= 0 {
+		return nil, http.StatusBadRequest, errNoBodyProvided
+	}
+	return body, http.StatusOK, nil
 }
 
 // helper - opens collection and gets a single user from database
