1e19a56559374aa2d4c50093ec239d7aeb407871
FredrickHZO <federico.armagno97@gmail.com>
2024-05-12 04:19:29 +0200

    Add delete route

--- back-end/main.go
+++ back-end/main.go
@@ -18,7 +18,8 @@ var (
 
 func handleTestRoutes() {
 	router.HandleFunc("/user", routes.GetUserHandler).Methods("GET")
-	router.HandleFunc("/user", routes.PutUserHandler).Methods("PUT")
+	router.HandleFunc("/user", routes.PutUserHandler).Methods("POST")
+	router.HandleFunc("/user", routes.DelUserHandler).Methods("DELETE")
 	router.HandleFunc("/user/list", routes.GetUsersListHandler).Methods("GET")
 }
 
