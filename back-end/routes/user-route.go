dd9a9d823cd38e655af181bf142723a95d09b749
FredrickHZO <federico.armagno97@gmail.com>
2024-05-14 18:21:19 +0200

    Test mongo query

--- back-end/routes/user-route.go
+++ back-end/routes/user-route.go
@@ -17,16 +17,42 @@ var (
 	EMAIL_KEY_JSON = "email"
 )
 
+/*
+GetUsersListHandler handles requests to retrieve a list of users based on query parameters.
+
+It converts the query parameters to a MongoDB query, retrieves the matching users from the database,
+and writes the result as a JSON response. If an error occurs, it responds with the appropriate error message and status code.
+
+@param w: The HTTP response writer
+@param r: The HTTP request
+*/
 func GetUsersListHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO: must work with any filter applied
+	query, err := db.ToMongoQuery(r.URL.Query())
+	if err != nil {
+		status := statusError(err)
+		res.Err(w, err, status)
+		return
+	}
+
 	var retrievedUserList []db.UserSchema
-	err := db.GetMany(db.USER_COLLECTION, &retrievedUserList)
+	err = db.GetMany(db.USER_COLLECTION, query, &retrievedUserList)
+
 	if err != nil {
 		res.Err(w, err, http.StatusInternalServerError)
+		return
 	}
 	res.Ok(w, retrievedUserList)
 }
 
+/*
+GetUserHandler handles requests to retrieve a single user based on their email.
+
+It retrieves the email from the query parameters, fetches the user from the database,
+and writes the result as a JSON response. If an error occurs, it responds with the appropriate error message and status code.
+
+@param w: The HTTP response writer
+@param r: The HTTP request
+*/
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	userEmail := r.URL.Query().Get(EMAIL_KEY_JSON)
 	if userEmail == "" {
@@ -49,6 +75,15 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	res.Ok(w, retrievedUser)
 }
 
+/*
+PutUserHandler handles requests to add a new user to the database.
+
+It reads the request body, unmarshals the JSON into a UserSchema, and inserts the user into the database.
+If an error occurs, it responds with the appropriate error message and status code.
+
+@param w: The HTTP response writer
+@param r: The HTTP request
+*/
 func PutUserHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := decodeRequestBody(r.Body)
 	if err != nil {
@@ -71,6 +106,15 @@ func PutUserHandler(w http.ResponseWriter, r *http.Request) {
 	res.Ok(w, nil)
 }
 
+/*
+DelUserHandler handles requests to delete a user from the database based on their email.
+
+It retrieves the email from the query parameters and deletes the user from the database.
+If an error occurs, it responds with the appropriate error message and status code.
+
+@param w: The HTTP response writer
+@param r: The HTTP request
+*/
 func DelUserHandler(w http.ResponseWriter, r *http.Request) {
 	userEmail := r.URL.Query().Get(EMAIL_KEY_JSON)
 	if userEmail == "" {
