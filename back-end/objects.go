04f56ed795b0c724cbd281f383404e1e729a86bd
FredrickHZO <federico.armagno97@gmail.com>
2024-05-25 11:16:40 +0200

    Add User fields validation

--- back-end/objects.go
+++ back-end/objects.go
@@ -1,20 +1,20 @@
 package main
 
 type User struct {
-	Email    string `bson:"_id" json:"email"`
-	Password string `bson:"password" validate:"required,password"`
-	Name     string `bson:"name,omitempty"`
-	Surname  string `bson:"surname,omitempty"`
-	Age      uint8  `bson:"age,omitempty"`
+	Email    string `bson:"_id" json:"email,omitempty" validate:"required,email"`
+	Password string `bson:"password" json:"password,omitempty" validate:"required"`
+	Name     string `bson:"name" json:"name,omitempty" validate:"required"`
+	Surname  string `bson:"surname" json:"surname,omitempty" validate:"required"`
+	Age      uint8  `bson:"age,omitempty" json:"age,omitempty"`
 }
 
 type Date struct {
-	Labels []string `bson:"label"`
+	Labels []string `bson:"labels,omitempty" json:"labels,omitempty"`
 	Type   string   `bson:"type"`
 
-	Year  string `bson:"year"`
-	Month string `bson:"month"`
-	Day   string `bson:"day"`
+	Year  uint16 `bson:"year"`
+	Month uint8  `bson:"month"`
+	Day   uint8  `bson:"day"`
 }
 
 type Calendar struct {
