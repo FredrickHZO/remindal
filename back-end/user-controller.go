ae8b00341fa6ae2bdfcfd812e0fac4e0cebfb0f3
FredrickHZO <federico.armagno97@gmail.com>
2024-05-26 14:03:32 +0200

    Use QueryBuilder

--- back-end/user-controller.go
+++ back-end/user-controller.go
@@ -9,31 +9,27 @@ import (
 	db "remindal/internal/database"
 
 	"github.com/go-playground/validator/v10"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 var (
-	EMAIL_KEY_DB   = "_id"
-	EMAIL_KEY_JSON = "email"
+	EMAIL_KEY = "_id"
 
 	errNoEmailProvided = errors.New("no email provided")
 	errInvalidUserInfo = errors.New("invalid or missing user info")
 )
 
-/*
-GetUsersListHandler handles requests to retrieve a list of users based on query parameters.
-
-It converts the query parameters to a MongoDB query, retrieves the matching users from the database,
-and writes the result as a JSON response. If an error occurs, it responds with the appropriate error message and status code.
-*/
+// Handles requests to retrieve a list of users based on query parameters.
+//
+// It converts the query parameters to a MongoDB query, retrieves the matching users from the database,
+// and writes the result as a JSON response. If an error occurs, it responds with the appropriate error message and status code.
 func GetUsersListHandler(w http.ResponseWriter, r *http.Request) {
-	//query := r.URL.Query()
+	query := r.URL.Query()
+	qbuilder := db.NewQueryBuilder()
+	constructUserQuery(query, &qbuilder)
 
 	var retrievedUserList []User
-	sort := bson.D{{Key: "age", Value: 1}}
-	query := bson.D{{}}
-
-	err := db.GetMany(db.USER_COLLECTION, query, sort, &retrievedUserList)
+	sort := db.CreateSort("age", 1)
+	err := db.GetMany(db.USER_COLLECTION, qbuilder.Query(), sort, &retrievedUserList)
 	if err != nil {
 		Eres(w, Err500(err))
 		return
@@ -41,22 +37,19 @@ func GetUsersListHandler(w http.ResponseWriter, r *http.Request) {
 	Okres(w, retrievedUserList)
 }
 
-/*
-GetUserHandler handles requests to retrieve a single user based on their email.
-
-It retrieves the email from the query parameters, fetches the user from the database,
-and writes the result as a JSON response. If an error occurs, it responds with the appropriate error message and status code.
-*/
+// GetUserHandler handles requests to retrieve a single user based on their email.
+//
+// It retrieves the email from the query parameters, fetches the user from the database,
+// and writes the result as a JSON response. If an error occurs, it responds with the appropriate error message and status code.
 func GetUserHandler(w http.ResponseWriter, r *http.Request) {
-	userEmail := r.URL.Query().Get(EMAIL_KEY_JSON)
+	userEmail := r.URL.Query().Get(EMAIL_KEY)
 	if userEmail == "" {
 		Eres(w, Err400(errNoEmailProvided))
 		return
 	}
 
 	var retrievedUser User
-	err := db.GetOne(db.USER_COLLECTION, EMAIL_KEY_DB,
-		userEmail, &retrievedUser)
+	err := db.GetOne(db.USER_COLLECTION, EMAIL_KEY, userEmail, &retrievedUser)
 	if err != nil {
 		Eres(w, Err400(err))
 		return
@@ -64,12 +57,10 @@ func GetUserHandler(w http.ResponseWriter, r *http.Request) {
 	Okres(w, retrievedUser)
 }
 
-/*
-PutUserHandler handles requests to add a new user to the database.
-
-It reads the request body, unmarshals the JSON into a UserSchema, and inserts the user into the database.
-If an error occurs, it responds with the appropriate error message and status code.
-*/
+// Handles requests to add a new user to the database.
+//
+// Reads the request body, unmarshals the JSON into a UserSchema, and inserts the user into the database.
+// Ifn error occurs, it responds with the appropriate error message and status code.
 func PutUserHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -91,6 +82,7 @@ func PutUserHandler(w http.ResponseWriter, r *http.Request) {
 		Eres(w, Err400(errInvalidUserInfo))
 		return
 	}
+
 	if err := db.PutOne(db.USER_COLLECTION, newuser); err != nil {
 		Eres(w, Err500(err))
 		return
@@ -98,20 +90,18 @@ func PutUserHandler(w http.ResponseWriter, r *http.Request) {
 	Okres(w, nil)
 }
 
-/*
-DelUserHandler handles requests to delete a user from the database based on their email.
-
-It retrieves the email from the query parameters and deletes the user from the database.
-If an error occurs, it responds with the appropriate error message and status code.
-*/
+// Handles requests to delete a user from the database based on their email.
+//
+// Retrieves the email from the query parameters and deletes the user from the database.
+// If an error occurs, it responds with the appropriate error message and status code.
 func DelUserHandler(w http.ResponseWriter, r *http.Request) {
-	userEmail := r.URL.Query().Get(EMAIL_KEY_JSON)
+	userEmail := r.URL.Query().Get(EMAIL_KEY)
 	if userEmail == "" {
 		Eres(w, Err400(errNoEmailProvided))
 		return
 	}
 
-	err := db.DeleteOne(db.USER_COLLECTION, EMAIL_KEY_DB, userEmail)
+	err := db.DeleteOne(db.USER_COLLECTION, EMAIL_KEY, userEmail)
 	if err != nil {
 		Eres(w, Err400(err))
 		return
