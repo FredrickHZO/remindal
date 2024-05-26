57f3747eaa8ae6611b2b28b2041363acff2760cb
FredrickHZO <federico.armagno97@gmail.com>
2024-05-26 14:02:40 +0200

    Add controller-utils

---
+++ back-end/controllers-utils.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/url"
+	db "remindal/internal/database"
+	"strconv"
+)
+
+var (
+	MULTI_SEL_SEPARATOR = ","
+)
+
+// Checks if all the possible User filters are inside the HTTP URL query
+func constructUserQuery(q url.Values, qb *db.QueryBuilder) {
+	email := q.Get("_id")
+	if email != "" {
+		qb.AddField("_id", email)
+	}
+	password := q.Get("password")
+	if password != "" {
+		qb.AddField("password", password)
+	}
+	names := q.Get("name")
+	if names != "" {
+		qb.AddMultiSelectField("name", names, MULTI_SEL_SEPARATOR)
+	}
+	surnames := q.Get("surname")
+	if surnames != "" {
+		qb.AddMultiSelectField("surname", surnames, MULTI_SEL_SEPARATOR)
+	}
+	minAge := q.Get("minage")
+	if minAge != "" {
+		qb.AddRangeFieldC("age", minAge, true, func(s string) (any, error) {
+			return strconv.Atoi(s)
+		})
+	}
+	maxAge := q.Get("maxage")
+	if maxAge != "" {
+		qb.AddRangeFieldC("age", maxAge, false, func(s string) (any, error) {
+			return strconv.Atoi(s)
+		})
+	}
+	age := q.Get("age")
+	if age != "" {
+		qb.AddFeildC("age", age, func(s string) (any, error) {
+			return strconv.Atoi(s)
+		})
+	}
+}
