5ce18eadf38e5091a916dcaa1f07d1f0ac6c3a62
FredrickHZO <federico.armagno97@gmail.com>
2024-05-26 14:02:06 +0200

    Add QueryBuilder - Refactor

--- back-end/internal/database/query.go
+++ back-end/internal/database/query.go
@@ -3,116 +3,105 @@ package database
 // TODO: Rework / remove
 
 import (
-	"net/url"
-	"strconv"
+	"errors"
+	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var (
-	keyMap = map[string]string{
-		"email": "_id", // userSchema
-		// ...
-	}
-
-	multiSelKeys = []string{"name"}    // test
-	minRangeKeys = []string{"min_age"} // test
-	maxRangeKeys = []string{"max_age"} // test
-)
-
-/*
-ToMongoQuery converts the query parameters from an HTTP request into a MongoDB compatible query.
-
-Iterates over the query parameters, identifies the type of filter (range, list, or single value),
-and constructs a corresponding BSON filter.
-*/
-func ToMongoQuery(query url.Values) (bson.D, error) {
-	queryDoc := bson.D{}
-	conds := bson.A{}
-	for k, v := range query {
-		k = getBsonDatabaseKey(k)
-		switch {
-		case contains(multiSelKeys, k):
-			queryDoc = append(queryDoc, multiSelFilter(k, v))
-
-		case contains(minRangeKeys, k):
-			filter, err := rangeFilter(k, v, "$gte")
-			if err != nil {
-				return bson.D{}, err
-			}
-			queryDoc = append(queryDoc, filter)
-
-		case contains(maxRangeKeys, k):
-			filter, err := rangeFilter(k, v, "$lte")
-			if err != nil {
-				return bson.D{}, err
-			}
-			queryDoc = append(queryDoc, filter)
-
-		default:
-			conds = append(conds, bson.D{{Key: k, Value: v}})
-		}
-	}
-	queryDoc = append(queryDoc, bson.E{Key: "$or", Value: conds})
-
-	return queryDoc, nil
+type QueryBuilder struct {
+	err   error
+	query bson.D
 }
 
-/*
-Converts the key name in the HTTP request to the corresponding database key name.
-*/
-func getBsonDatabaseKey(k string) string {
-	if item, ok := keyMap[k]; ok {
-		return item
+func NewQueryBuilder() QueryBuilder {
+	return QueryBuilder{
+		err:   nil,
+		query: bson.D{},
 	}
-	return k
 }
 
-/*
-Handles a multiple selection in the HTTP request. A multiple selection takes the form:
+// adds a simple field filter to the query document
+func (qb *QueryBuilder) AddField(k string, v string) {
+	qb.query = append(qb.query, bson.E{Key: k, Value: v})
+}
 
-	key=valkey=val2key=val3key=val4key=val5
-
-Splits the string containing the values and creates the appropriate query using the "$or" operator.
-*/
-func multiSelFilter(k string, v []string) primitive.E {
-	var orConditions bson.A
-	for _, item := range v {
+// adds a multi select filter to the query document
+func (qb *QueryBuilder) AddMultiSelectField(k string, v string, sep string) {
+	vals := strings.Split(v, sep)
+	orConditions := bson.A{}
+	for _, item := range vals {
 		orConditions = append(orConditions, bson.D{{Key: k, Value: item}})
 	}
-	return primitive.E{Key: "$or", Value: orConditions}
+	qb.query = append(qb.query, bson.E{Key: "$or", Value: orConditions})
 }
 
-/*
-contains checks if a specific key is present in an array of keys.
-
-Validates that a key from the HTTP request is allowed
-to have certain types of values (e.g., multiple selection or range values).
-By ensuring the key is within the predefined allowed keys, it helps maintain
-the integrity of the query formation process.
-*/
-func contains(allowedKeys []string, k string) bool {
-	for _, item := range allowedKeys {
-		if item == k {
-			return true
-		}
-	}
-	return false
-}
-
-/*
-Reimplement
-*/
-func rangeFilter(k string, v []string, c string) (bson.E, error) {
-	val, err := strconv.Atoi(v[0])
-	if err != nil {
-		return bson.E{}, err
-	}
-
+// adds a range filter to the query document
+func (qb *QueryBuilder) AddRangeField(k string, v string, c string) {
 	rangeCond := bson.E{
 		Key:   k,
-		Value: bson.D{bson.E{Key: c, Value: val}},
+		Value: bson.D{bson.E{Key: c, Value: v}},
 	}
-	return rangeCond, nil
+	qb.query = append(qb.query, rangeCond)
+}
+
+// converts the value and adds a simple field filter to the query document
+// adds an error to the QueryBuilder if the convertion is unsuccessfull
+func (qb *QueryBuilder) AddFeildC(k string, v string, cnv func(s string) (any, error)) {
+	val, err := cnv(v)
+	if err != nil {
+		qb.err = errors.Join(err)
+		return
+	}
+	qb.query = append(qb.query, bson.E{Key: k, Value: val})
+
+}
+
+// converts the values and adds a multi select filter to the query document
+// adds an error to the QueryBuilder if the convertion is unsuccessfull
+func (qb *QueryBuilder) AddMultiSelectC(k string, v string, sep string, cnv func(s string) (any, error)) {
+	vals := strings.Split(v, sep)
+
+	orConditions := bson.A{}
+	for _, item := range vals {
+		val, err := cnv(item)
+		if err != nil {
+			qb.err = errors.Join(err)
+			return
+		}
+		orConditions = append(orConditions, bson.D{{Key: k, Value: val}})
+	}
+	qb.query = append(qb.query, bson.E{Key: "$or", Value: orConditions})
+}
+
+// converts the value and adds a range filter to the query document
+// adds an error to the QueryBuilder if the convertion is unsuccessfull
+func (qb *QueryBuilder) AddRangeFieldC(k string, v string, min bool, cnv func(s string) (any, error)) {
+	val, err := cnv(v)
+	if err != nil {
+		qb.err = errors.Join(err)
+		return
+	}
+
+	var cond string
+	if min {
+		cond = "$gte"
+	} else {
+		cond = "$lte"
+	}
+	rangeCond := bson.E{
+		Key:   k,
+		Value: bson.D{bson.E{Key: cond, Value: val}},
+	}
+	qb.query = append(qb.query, rangeCond)
+}
+
+// returns the error in the QueryBuilder
+func (qb *QueryBuilder) Err() error {
+	return qb.err
+}
+
+// return the built query
+func (qb *QueryBuilder) Query() bson.D {
+	return qb.query
 }
