b941fa8ca0e6f1335abe2e2c4294b5f042f0a907
FredrickHZO <federico.armagno97@gmail.com>
2024-05-29 22:50:28 +0200

    Add day validation in calendar

--- back-end/objects.go
+++ back-end/objects.go
@@ -1,6 +1,11 @@
 package main
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"github.com/go-playground/validator"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
 	Email    string `bson:"_id" json:"_id,omitempty" validate:"required,email"`
@@ -10,7 +15,6 @@ type User struct {
 	Age      uint8  `bson:"age,omitempty" json:"age,omitempty"`
 }
 
-// TODO: better validation
 type Date struct {
 	ID primitive.ObjectID `bson:"_id" json:"_id"`
 
@@ -19,9 +23,30 @@ type Date struct {
 
 	Year    uint16 `bson:"year" json:"year" validate:"required"`
 	Month   uint8  `bson:"month" json:"month" validate:"required,min=0,max=12"`
-	Day     uint8  `bson:"day" json:"day" validate:"required,min=1,max=31"`
+	Day     uint8  `bson:"day" json:"day" validate:"required,dayValidation"`
 	Hours   uint8  `bson:"hours,omitempty" json:"hours,omitempty" validate:"min=0,max=23"`
 	Minutes uint8  `bson:"minutes,omitempty" json:"minutes,omitempty" validate:"min=0,max=59"`
+}
+
+// Checks if the date is a valid one by getting the total days in
+// the specified month and year and comparing the day in input with
+// the total days value. If the input day value is less then 0 or greater
+// than the total days in that given month, return false, true otherwise
+func dayValidation(fl validator.FieldLevel) bool {
+	year := fl.Parent().FieldByName("Year").Int()
+	month := fl.Parent().FieldByName("Month").Int()
+	day := fl.Parent().FieldByName("Day").Int()
+
+	t := time.Date(int(year), time.Month(month), 32, 0, 0, 0, 0, time.UTC)
+	totalDays := 32 - t.Day()
+
+	if day < 0 {
+		return false
+	}
+	if int(day) > totalDays {
+		return false
+	}
+	return true
 }
 
 type Calendar struct {
