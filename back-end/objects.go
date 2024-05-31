433d10d09ea84d014b8b745927c824a12f1f94bc
FredrickHZO <federico.armagno97@gmail.com>
2024-05-31 12:10:12 +0200

    Fix Date validation bug

--- back-end/objects.go
+++ back-end/objects.go
@@ -3,8 +3,7 @@ package main
 import (
 	"time"
 
-	"github.com/go-playground/validator"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/go-playground/validator/v10"
 )
 
 type User struct {
@@ -16,16 +15,17 @@ type User struct {
 }
 
 type Date struct {
-	ID primitive.ObjectID `bson:"_id" json:"_id"`
+	ID string `bson:"_id,omitempty" json:"_id,omitempty"`
 
 	Labels []string `bson:"labels,omitempty" json:"labels,omitempty"`
 	Type   string   `bson:"type" json:"type" validate:"required"`
+	Desc   string   `bson:"description,omitempty" json:"description,omitempty"`
 
-	Year    uint16 `bson:"year" json:"year" validate:"required"`
-	Month   uint8  `bson:"month" json:"month" validate:"required,min=1,max=12"`
-	Day     uint8  `bson:"day" json:"day" validate:"required,dayValidation"`
-	Hours   uint8  `bson:"hours,omitempty" json:"hours,omitempty" validate:"min=0,max=23"`
-	Minutes uint8  `bson:"minutes,omitempty" json:"minutes,omitempty" validate:"min=0,max=59"`
+	Year    int16 `bson:"year" json:"year" validate:"required"`
+	Month   int8  `bson:"month" json:"month" validate:"required,min=1,max=12"`
+	Day     int8  `bson:"day" json:"day" validate:"required,day_validation"`
+	Hours   int8  `bson:"hours,omitempty" json:"hours,omitempty" validate:"min=0,max=23"`
+	Minutes int8  `bson:"minutes,omitempty" json:"minutes,omitempty" validate:"min=0,max=59"`
 }
 
 // Checks if the date is a valid one by getting the total days in
@@ -35,10 +35,10 @@ type Date struct {
 func dayValidation(fl validator.FieldLevel) bool {
 	year := fl.Parent().FieldByName("Year").Int()
 	month := fl.Parent().FieldByName("Month").Int()
-	day := fl.Parent().FieldByName("Day").Int()
+	day := fl.Field().Int()
 
-	// 32 is greater than any possible value, it will get automatically normalized.
-	// 2024-02-32 becomes 2024-03-04. 32 - 4 = 29
+	// 32 is greater than any possible value, it will be automatically normalized.
+	// 2024-02-32 becomes 2024-03-03 after normalization. 32 - 3 = 29
 	t := time.Date(int(year), time.Month(month), 32, 0, 0, 0, 0, time.UTC)
 	maxDays := 32 - t.Day()
 
@@ -46,6 +46,13 @@ func dayValidation(fl validator.FieldLevel) bool {
 		return false
 	}
 	return true
+}
+
+// returns a new validator with registered custom validators for the object Date
+func newCustomDateValidator() *validator.Validate {
+	validate := validator.New()
+	validate.RegisterValidation("day_validation", dayValidation)
+	return validate
 }
 
 type Calendar struct {
